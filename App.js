2a049776bcf30fe2b7f67be658f5ddbe1b0093f0
James Hughes <jameshughes@MagenTyss-MacBook-Pro-2.local>
2018-07-02 17:01:19 +0100

    changed scheme to Debug, FL lane to Debug and added libraries to Xcode IDE General

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+// import { TEXT } from 'react-native-dotenv';
 
 export default class App extends React.Component {
   render() {
